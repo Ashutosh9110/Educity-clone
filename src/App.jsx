87171297fa643028d814e6ee51b35d75bc08b192
Ashutosh9110 <ashusingh19911082@gmail.com>
2025-02-19 10:53:20 +0530

    almost done

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Title from "./components/title/title";
 import Campus from "./components/campus/Campus";
 import Contact from "./components/contact/Contact";
 import Programs from "./components/programs/Programs";
+import Testimonials from "./components/testimonials/Testimonials";
 
 
 function App() {
@@ -20,6 +21,8 @@ function App() {
         <About setPlayState={setPlayState}/>
         <Title subTitle="Gallery" title="Campus Photos" />
         <Campus />
+        <Title subTitle="Testimonials" title="What student says" />
+        <Testimonials  />
         <Title subTitle="Contact Us" title="Get in touch" />
         <Contact />
         <Footer />
