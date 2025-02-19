e7e22c71e69b4ccbcd72284856be8022478fffb4
Ashutosh9110 <ashusingh19911082@gmail.com>
2025-02-19 10:44:08 +0530

    added another component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
 import React, { useState } from "react";
 import Navbar from "./components/navbar/Navbar";
+import Hero from "./components/hero/Hero";
 
 import About from "./components/about/About";
+import Title from "./components/title/title";
 
 
 function App() {
   return (
     <div>
       <Navbar />
+      <Hero />
       <div className="container">
         <About setPlayState={setPlayState}/>
+        <Title subTitle="Contact Us" title="Get in touch" />
+        <Footer />
       </div>
     </div>
   )
