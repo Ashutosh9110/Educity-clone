3a60110359b90914306183d1ff3927de3128d974
Ashutosh9110 <ashusingh19911082@gmail.com>
2025-02-19 10:47:52 +0530

    added contact section

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Hero from "./components/hero/Hero";
 
 import About from "./components/about/About";
 import Title from "./components/title/title";
+import Campus from "./components/campus/Campus";
+import Contact from "./components/contact/Contact";
 
 
 function App() {
@@ -13,7 +15,10 @@ function App() {
       <Hero />
       <div className="container">
         <About setPlayState={setPlayState}/>
+        <Title subTitle="Gallery" title="Campus Photos" />
+        <Campus />
         <Title subTitle="Contact Us" title="Get in touch" />
+        <Contact />
         <Footer />
       </div>
     </div>
